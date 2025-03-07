1712250cc06a61321b65b4b3e8a367a5db37d075
Dillon Phang <dillondayo@gmail.com>
2025-03-07 18:40:12 +0800

    Completed exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(seq) {
+	if (seq == 0) {
+		return 0;
+	} else if (seq < 0) {
+		return "OOPS";
+	}
+	let fib = [1, 1];
 
+	if (seq > 2) {
+		let first = 0;
+		let second = 1;
+		for (let i = seq - 2; i > 0; i--) {
+			fib.push(fib[first] + fib[second]);
+			first++;
+			second++;
+		}
+	}
+	return fib[seq - 1];
 };
 
 // Do not edit below this line
