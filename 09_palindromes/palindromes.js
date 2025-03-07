a0a20f360fb815cb8c1a7a656e9b6db76d4d74f4
Dillon Phang <dillondayo@gmail.com>
2025-03-07 18:28:40 +0800

    Completed exercise 9, palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const parsedStr = str.toLowerCase().split('')
+						.filter(c => ((c >= 'a' && c <= 'z') 
+									|| (c >= '0' && c <= '9')))
+						.join('');
+	const reversedStr = parsedStr.split('').reverse().join('');
+	
+	if (parsedStr != reversedStr) {
+		return false;
+	}
+	return true;
 };
 
 // Do not edit below this line
