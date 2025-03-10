7127daab8985fcf89328281469426e3ede15b2ef
Dillon Phang <dillondayo@gmail.com>
2025-03-10 21:17:59 +0800

    Completed exercise 12, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+	const currentYear = (new Date()).getFullYear();
 
+	arr.sort((a, b) => {
+		if (!a.yearOfDeath || !b.yearOfDeath) {
+			return (currentYear - b.yearOfBirth) - (currentYear - a.yearOfBirth);
+		}
+		return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+	});
+	return arr[0];
 };
 
 // Do not edit below this line
